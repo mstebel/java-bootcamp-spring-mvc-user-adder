55af3a652eb547a18491e64b966c4c1cc0b59310
Magda <m.stebel@mailplus.pl>
2022-04-18 17:17:38 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/ms/springmvc_20_1/Springmvc201Application.java
@@ -0,0 +1,13 @@
+package pl.ms.springmvc_20_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springmvc201Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springmvc201Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ms/springmvc_20_1/Springmvc201ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ms.springmvc_20_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springmvc201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
